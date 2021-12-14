d9e21269e4f0d5e45e8a5217f0fcbd73147e486e
Juan Marco <jmmarco@gmail.com>
2021-12-14 10:08:32 +0100

    Display available suggestions

--- src/App.js
+++ src/App.js
@@ -32,26 +32,60 @@ class App extends React.Component {
         sampleSuggestions
       );
 
-      console.log('autocomplete', autoCompleteResult)
+      console.log("autocomplete", autoCompleteResult);
 
       autoCompleteResult
-      ? this.setState({
-          suggestionsList: autoCompleteResult,
-          activeSuggestion: true,
-          lastWord,
-        })
-      : this.setState((state) => {
-          return {
-            suggestion: "",
-            suggestionsList: [],
-            activeSuggestion: false,
-            lastWord: "",
-          };
-        });
+        ? this.setState({
+            suggestionsList: autoCompleteResult,
+            activeSuggestion: true,
+            lastWord,
+          })
+        : this.setState((state) => {
+            return {
+              suggestion: "",
+              suggestionsList: [],
+              activeSuggestion: false,
+              lastWord: "",
+            };
+          });
     }
   };
 
+  handleSuggestion = () => {
+    const { inputText, suggestion, activeSuggestion } = this.state;
+
+    // if no suggestion is available simply return
+    if (!suggestion || !activeSuggestion) return;
+
+    // split the input
+    const splittedInput = inputText.split(" ");
+    // remove the last item
+    splittedInput.pop();
+    // add the suggestion
+    splittedInput.push(suggestion);
+
+    // set the input text back and clear suggestion box
+    this.setState({
+      inputText: splittedInput.join(" "),
+      suggestion: "",
+      activeSuggestion: false,
+    });
+  };
+
+  setSuggestion = (suggestion) => {
+    // Because setState is async, we need to pass a suggestion
+    this.setState(
+      {
+        suggestion,
+      },
+      () => {
+        this.handleSuggestion();
+      }
+    );
+  };
+
   render() {
+    const { activeSuggestion, suggestionsList } = this.state;
     return (
       <div className="App">
         <h1>Autocomplete app</h1>
@@ -63,6 +97,19 @@ class App extends React.Component {
             onChange={this.handleChange}
             value={this.state.inputText}
           ></textarea>
+
+          <div className="ui-suggestion-container">
+            {activeSuggestion
+              ? suggestionsList.map((suggestion) => (
+                  <span
+                    className="suggestion"
+                    onClick={() => this.setSuggestion(suggestion)}
+                  >
+                    {suggestion}
+                  </span>
+                ))
+              : null}
+          </div>
         </div>
       </div>
     );
