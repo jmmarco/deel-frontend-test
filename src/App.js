b13f74201dadcecb561bc5c0668836914a348939
Juan Marco <jmmarco@gmail.com>
2021-12-14 09:33:48 +0100

    Set textarea as a controlled component

--- src/App.js
+++ src/App.js
@@ -2,8 +2,27 @@ import "./App.css";
 import React from "react";
 
 class App extends React.Component {
+  state = {
+    inputText: "",
+  };
+
+  handleChange = (e) => {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
   render() {
-    return <div className="App"><h1>Autocomplete app</h1></div>;
+    return (
+      <div className="App">
+        <h1>Autocomplete app</h1>
+        <div>{JSON.stringify(this.state)}</div>
+        <div className="ui-container">
+          <textarea className="textarea" name="inputText" onChange={this.handleChange} value={this.state.inputText}></textarea>
+        </div>
+      </div>
+    );
   }
 }
 
