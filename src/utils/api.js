b45d7b02ea1aaba0872453b7d33a35454998b7e1
Juan Marco <jmmarco@gmail.com>
2021-12-14 11:12:20 +0100

    Add fetchRealRandomWordsApi

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,10 +3,25 @@ export default async function autocomplete(inputStr = "", inputArr = []) {
     // Use setTimeout to simulate "asynchronous" feel
     setTimeout(() => {
       if (inputStr.length > 1 && Array.isArray(inputArr)) {
-        resolve(inputArr.filter((item) => item.toLowerCase().indexOf(inputStr.toLowerCase()) !== -1));
+        resolve(
+          inputArr.filter(
+            (item) => item.toLowerCase().indexOf(inputStr.toLowerCase()) !== -1
+          )
+        );
       } else {
-        resolve(undefined)
+        resolve(undefined);
       }
     }, 400);
   });
+}
+
+export async function fetchRealRandomWordsApi(wordsAmount = 5) {
+  const response = await fetch(
+    `https://random-word-api.herokuapp.com/word?number=${wordsAmount}`
+  );
+  if (!response.ok) {
+    const message = `An error has occured: ${response.status} please try again later.`;
+    throw new Error(message);
+  }
+  return await response.json();
 }
