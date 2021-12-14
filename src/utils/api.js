606aca378cbd6d0af6e6460e48c78762111c192e
Juan Marco <jmmarco@gmail.com>
2021-12-14 09:43:11 +0100

    Add comment

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,6 @@
 export default async function autocomplete(inputStr = "", inputArr = []) {
   return await new Promise((resolve, reject) => {
+    // Use setTimeout to simulate "asynchronous" feel
     setTimeout(() => {
       if (inputStr.length > 1 && Array.isArray(inputArr)) {
         resolve(inputArr.filter((item) => item.toLowerCase().indexOf(inputStr.toLowerCase()) !== -1));
